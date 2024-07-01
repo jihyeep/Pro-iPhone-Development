2952fea62e0e696863e53b7168350d5d98f5bb3b
Jihye Park <deviloper.park@gmail.com>
2024-07-01 11:58:16 +0900

    Closure 문법 생략 및 다중 파라미터 closure

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -19,3 +19,48 @@ let y = { (x: Int) -> Int in return x * 2 }
 
 print(y(4))
 print(y(17))
+
+// 클로저 인풋, 아웃풋 인자 생략
+let z = { x in return x * 2 }
+
+print(z(4))
+print(z(17))
+
+// return 키워드 생략
+let w = { x in x * 2} /// 타입 추론
+
+print(w(4))
+print(w(17))
+
+// 인풋 arguments 이름 생략
+let v = { $0 * 2 }
+
+print(v(4))
+print(v(17))
+
+func addNumbers (x: Int, y: Int) -> Int {
+    return x + y
+}
+
+// 인풋 인자 타입 생략
+let a = { (x, y) -> Int in return x + y }
+
+print(a(4,5))
+print(a(17,19))
+
+// return 타입 생략
+let b = { (x: Int, y: Int) in return x + y }
+
+print(b(4,5))
+print(b(17,19))
+
+let c = { (x: Int, y: Int) in x + y }
+
+print(c(4,5))
+print(c(17,19))
+
+// 모호성이 있는 경우, 매개변수의 데이터 유형을 명시적으로 정의해야 함
+let d = { $0 as Int + $1 as Int }
+
+print(d(4,5))
+print(d(17,19))
