3ed8849e5afbfeeb47b6418f76d07b2dddaa85d1
Jihye Park <deviloper.park@gmail.com>
2024-07-01 11:10:53 +0900

    Debugging 프로젝트 추가 및 Closure 플레이그라운드 생성

---
+++ Debugging/Debugging/DebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingApp.swift
+//  Debugging
+//
+//  Created by 박지혜 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
