8617380258a49d397d1fcb9f85c8e70c039bf425
Jihye Park <deviloper.park@gmail.com>
2024-07-01 11:12:26 +0900

    Simple closure

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 전통적인 함수 정의
+func descriptiveName() {
+    print("Run!")
+}
+
+// 전통적인 함수 실행
+descriptiveName()
+
+func multiplyBy2 (x: Int) -> Int {
+    return x * 2
+}
+
+print(multiplyBy2(x: 4))
+print(multiplyBy2(x: 17))
+
+let y = { (x: Int) -> Int in return x * 2 }
+
+print(y(4))
+print(y(17))
