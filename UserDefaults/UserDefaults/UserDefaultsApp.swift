3e2e21a8df24c0531f04d3f4c1432fb6ad3fc976
Jihye Park <deviloper.park@gmail.com>
2024-07-02 10:05:27 +0900

    UserDefaults 프로젝트 추가

---
+++ UserDefaults/UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by 박지혜 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
