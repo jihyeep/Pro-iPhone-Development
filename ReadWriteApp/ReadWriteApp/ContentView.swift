f8bea11c67c40f23c83675e45be0bee28f42aeff
Jihye Park <deviloper.park@gmail.com>
2024-07-02 10:46:49 +0900

    TextField focus 및 placeHolder 설정

--- ReadWriteApp/ReadWriteApp/ContentView.swift
+++ ReadWriteApp/ReadWriteApp/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var createText = ""
+    let placeHolder = "텍스트를 입력하세요."
+    @State var createText = "텍스트를 입력하세요."
     @State var displayText = ""
+    
+    @FocusState var textFieldFocus: Bool
     
     var body: some View {
         VStack {
             TextEditor(text: $createText)
+                .foregroundStyle(placeHolder == createText ? .gray : .black)
+                .focused($textFieldFocus)
+                .onChange(of: textFieldFocus) {
+                    /* textField에 포커스 이동 시 placeHolder가 표시되어 있으면,
+                       입력 textField를 초기화하고 입력 대기 상태로 만듦 */
+                    if textFieldFocus, placeHolder == createText {
+                        createText = ""
+                    /* 다른 입력으로 포커스 이동 시 입력한 텍스트가 없으면,
+                       placeHoler 기본 텍스트로 대치 */
+                    } else if !textFieldFocus, createText.isEmpty {
+                        createText = placeHolder
+                    }
+                }            
             HStack {
                 Button(action: {
                     let fm = FileManager.default
@@ -31,6 +47,7 @@ struct ContentView: View {
                     Text("Write File")
                 }
                 Button(action: {
+                    // FileManager는 불필요한 데이터 저장일 수 있기 때문에 쓸 때마다 불러오는 것이 좋음
                     let fm = FileManager.default
                     let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
                     let url = urls.last?.appendingPathComponent("file.txt")
