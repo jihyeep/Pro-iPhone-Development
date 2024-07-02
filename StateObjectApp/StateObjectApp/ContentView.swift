9d85fe01a0bb10310b1be99fa2bb0f17d5bf45a7
Jihye Park <deviloper.park@gmail.com>
2024-07-02 16:24:45 +0900

    StateObjectApp 프로젝트 생성 및 StateObject 데이터 연동

---
+++ StateObjectApp/StateObjectApp/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  StateObjectApp
+//
+//  Created by 박지혜 on 7/2/24.
+//
+
+import SwiftUI
+
+class AnimalModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var breed: String = ""
+    @Published var age: Double = 0.0
+    @Published var weight: Double = 0.0
+}
+
+struct ContentView: View {
+    @StateObject var cat = AnimalModel()
+    
+    var body: some View {
+        VStack {
+            Text("Hello, \(cat.name)")
+            Text("Breed, \(cat.breed)")
+            Text("Age, \(cat.age)")
+            Text("Weight, \(cat.weight)")
+            
+            DisplayTextField(creature: cat)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
