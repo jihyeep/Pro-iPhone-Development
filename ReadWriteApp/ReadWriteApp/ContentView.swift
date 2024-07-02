cf62f5a70d4ce06cc914e89825bd656e15b7ddf4
Jihye Park <deviloper.park@gmail.com>
2024-07-02 10:08:15 +0900

    ReadWriteApp 프로젝트 생성 및 UI 작업

---
+++ ReadWriteApp/ReadWriteApp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  ReadWriteApp
+//
+//  Created by 박지혜 on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var createText = ""
+    @State var displayText = ""
+    
+    var body: some View {
+        VStack {
+            TextEditor(text: $createText)
+            HStack {
+                Button(action: {}) {
+                    Text("Write File")
+                }
+                Button(action: {}) {
+                    Text("Read File")
+                }
+            }
+            .padding()
+            TextEditor(text: $displayText)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
