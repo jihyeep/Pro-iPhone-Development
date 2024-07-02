5dca49629296178099e70c59e76e67c767e32b70
Jihye Park <deviloper.park@gmail.com>
2024-07-02 16:07:34 +0900

    Binding 파일 분리 및 프리뷰에 constant 사용

--- BindingApp/BindingApp/ContentView.swift
+++ BindingApp/BindingApp/ContentView.swift
@@ -23,13 +23,4 @@ struct ContentView: View {
     ContentView()
 }
 
-struct DisplayTextField: View {
-    @Binding var newVariable: String
-    
-    var body: some View {
-        HStack {
-            Text("Send a greeting:")
-            TextField("Type a message here", text: $newVariable)
-        }
-    }
-}
+
