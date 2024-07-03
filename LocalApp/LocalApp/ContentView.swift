ffcc22ac0bbfa0dd19c335fbdba9d56a21a9e7d9
Jihye Park <deviloper.park@gmail.com>
2024-07-03 10:33:31 +0900

    문자열 보간

--- LocalApp/LocalApp/ContentView.swift
+++ LocalApp/LocalApp/ContentView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State var changeColor = false
+    @State var message = ""
+
+    var placeHolder: LocalizedStringKey = "placeholder-label"
     
     var body: some View {
         VStack {
-            // Text, Button
-            Text(LocalizedStringKey("greeting-label"))
-            Text("greeting-label")
+            Text(LocalizedStringKey("greeting-label \(message)"))
+            // Text, Button은 LocalizedStringKey 를 직접 넣을 수 있다.
+            Text("greeting-label \(message)")
             Button("button-label") {
                 changeColor.toggle()
             }
+            TextField(placeHolder, text: $message)
         }
         .padding()
         .background(changeColor ? Color.red : Color.yellow)
     }
 }
 
+// 언어 미리보기 설정
 #Preview("기본 - 한국어") {
     ContentView()
 }
