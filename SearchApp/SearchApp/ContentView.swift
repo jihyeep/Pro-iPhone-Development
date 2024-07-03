bfad25fc5e12644c9859c652e42a3095f4aaf42b
Jihye Park <deviloper.park@gmail.com>
2024-07-03 13:33:21 +0900

    SearchScope

--- SearchApp/SearchApp/ContentView.swift
+++ SearchApp/SearchApp/ContentView.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
+enum ProductScope {
+    case fruit
+    case vegetable
+}
+
 struct ContentView: View {
     @State var searchText = ""
+    @State private var scope: ProductScope = .fruit
     
     let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
+    
+    let scopes = ["All", "Favorites", "Recent"]
     
     var body: some View {
         NavigationStack {
             PetListView(animals: petArray)
-                .navigationTitle("SearchAPp")
+                .navigationTitle("SearchApp")
         }
         .navigationBarTitleDisplayMode(.automatic)
         .searchable(text: $searchText,
@@ -23,9 +31,10 @@ struct ContentView: View {
                     prompt: "Look for a pet") {
             
             // suggestions
-            Text("Singing").searchCompletion("Canary")
-            Text("Croaking").searchCompletion("Frog")
-            Text("Grumpy").searchCompletion("Cat")
+//            Text("Singing").searchCompletion("Canary")
+//            Text("Funny").searchCompletion("Canary")
+//            Text("Croaking").searchCompletion("Frog")
+//            Text("Grumpy").searchCompletion("Cat")
             Spacer() // searchable의 리스트를 가려서 검색창을 눌렀을 때도 리스트가 안보이게 됨(아래 !searchText.isEmpty && 도 줘야 함)
             
             // 앞부터 매칭이 되는 것만 필터링
@@ -35,6 +44,10 @@ struct ContentView: View {
                 Text(name)
             }
         }
+                    .searchScopes($scope) {
+                        Text("Fruit").tag(ProductScope.fruit)
+                        Text("Vegetable").tag(ProductScope.vegetable)
+                    }
     }
 }
 
