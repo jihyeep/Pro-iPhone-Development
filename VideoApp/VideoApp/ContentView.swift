d9f2bde6334b390d2c301a54e5990dedf315d46b
Jihye Park <deviloper.park@gmail.com>
2024-07-05 11:03:59 +0900

    Video overlay

--- VideoApp/VideoApp/ContentView.swift
+++ VideoApp/VideoApp/ContentView.swift
@@ -12,7 +12,13 @@ struct ContentView: View {
     @State var player: AVPlayer?
     
     var body: some View {
-VideoPlayer(player: player)
+        VideoPlayer(player: player) {
+            // 비디오 플레이어 영역 위에 view 작업
+            VStack {
+                Text("Overlay text to appear")
+                    .foregroundStyle(.white)
+            }
+        }
             .frame(height: 320)
             .onAppear {
                 guard let videoURL = Bundle.main.url(forResource: "SaturnV", withExtension: "mov") else {
