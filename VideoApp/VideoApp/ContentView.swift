be7e1d5ee465926536ef9f245c2e2f8f53b66221
Jihye Park <deviloper.park@gmail.com>
2024-07-05 10:49:03 +0900

    VideoApp 프로젝트 생성 및 AVPlayer 추가

---
+++ VideoApp/VideoApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  VideoApp
+//
+//  Created by 박지혜 on 7/5/24.
+//
+
+import SwiftUI
+import AVKit
+
+struct ContentView: View {
+    @State var player: AVPlayer?
+    
+    var body: some View {
+VideoPlayer(player: player)
+            .frame(height: 320)
+            .onAppear {
+                guard let videoURL = Bundle.main.url(forResource: "SaturnV", withExtension: "mov") else {
+                    print("Video file not found")
+                    return
+                }
+                player = AVPlayer(url: videoURL as URL)
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoApp/VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by 박지혜 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
