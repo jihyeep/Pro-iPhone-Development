2c9b07b43e330088d4f40a97a6f722764a11d1ee
Jihye Park <deviloper.park@gmail.com>
2024-07-08 11:26:45 +0900

    다양한 음성 기능 추가

--- Text2SpeechApp/Text2SpeechApp/ContentView.swift
+++ Text2SpeechApp/Text2SpeechApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     @State var convertText = AVSpeechUtterance(string: "")
     @State var textToRoad = "This is a test of the emergency broadcast system"
-    @State var sliderValue: Float = 0.0 /// 음성 속도
+    @State var sliderValue: Float = 0.5 /// 음성 속도
     
     var body: some View {
         VStack {
@@ -23,6 +23,13 @@ struct ContentView: View {
             Button {
                 convertText = AVSpeechUtterance(string: textToRoad)
                 convertText.rate = sliderValue
+                
+                // 다양한 음성 지원 (안되는 것도 있음)
+                // https://gist.github.com/Koze/d1de49c24fc28375a9e314c72f7fdae4
+                let voice = AVSpeechSynthesisVoice(language: "en-GB")
+//                let voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.siri_male_en-GB_compact")
+                convertText.voice = voice
+                
                 audio.speak(convertText)
             } label: {
                 Text("Read Text Out Loud")
