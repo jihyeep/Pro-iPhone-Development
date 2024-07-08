06b8976c2a9cd83fbe411f67a823cc57c71da13d
Jihye Park <deviloper.park@gmail.com>
2024-07-08 13:36:26 +0900

    스토리보드 삭제 및 NavigationController 추가

---
+++ UIKitCodeonly/UIKitCodeonly/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UIKitCodeonly
+//
+//  Created by 박지혜 on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "ViewController"
+        
+    }
+
+
+}
+
