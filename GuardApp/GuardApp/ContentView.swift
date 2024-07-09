b530cf08ee3df142b4be1c5be4902f002cb14309
Jihye Park <deviloper.park@gmail.com>
2024-07-09 15:15:12 +0900

    if 구문으로 오류 방지(전통적인 방법)

---
+++ GuardApp/GuardApp/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  GuardApp
+//
+//  Created by 박지혜 on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State var showImage: UIImage = UIImage(systemName: "photo")!
+
+    var body: some View {
+        VStack {
+            Image(uiImage: showImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 400)
+            Button {
+                getImage("xyz")
+            } label: {
+                Text("Add Picture")
+            }
+        }
+        .padding()
+    }
+
+    func getImage(_ imageName: String) {
+        // 전통적인 방법
+        if UIImage(named: imageName) != nil {
+            showImage = UIImage(named: imageName)!
+        } else {
+            showImage = UIImage(systemName: "xmark.octagon.fill")!
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
